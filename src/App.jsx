660ae1f72cb1d51430391fa0de24916c0c69646d
Adnane <rchidiadnan1@gmail.com>
2026-02-24 11:29:56 +0000

    Making a header component with a dark and light mode button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Header from './components/Header'
+import { createContext, useState } from 'react'
+
+export const ThemeContext = createContext({});
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <>
-      
-    </>
+    <ThemeContext value={{darkMode, setDarkMode}}>
+      <Header />
+    </ThemeContext>
   )
 }
 
