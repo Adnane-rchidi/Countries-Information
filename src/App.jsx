bbc89273ae182d201d94ea8822f1b880de41ca72
Adnane <rchidiadnan1@gmail.com>
2026-03-01 12:10:02 +0000

    Finishing the app making CountryCard and SearchFilter components also making pages CountryDetails and Home and using riouter dom to go from page to page and style in app css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import './App.css'
-import Header from './components/Header'
-import { createContext, useState } from 'react'
+import "./App.css";
+import Header from "./components/Header";
+import { createContext, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CountryDetail from "./pages/CountryDetail";
 
 export const ThemeContext = createContext({});
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <ThemeContext value={{darkMode, setDarkMode}}>
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
       <Header />
-    </ThemeContext>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/country/:name" element={<CountryDetail />} />
+      </Routes>
+    </ThemeContext.Provider>
+  );
 }
 
-export default App
+export default App;
